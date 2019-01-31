ce376c534a7642b83aa4baa8414a23e21badd48b
Abdallah Drihem <samidrihem@gmail.com>
2019-01-31 17:29:26 +0100

    - Add LEVEL

---
+++ app/src/main/java/com/example/sami/labyrinthem2ihm/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sami.labyrinthem2ihm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLevel1;
+    Button btnLevel2;
+    Button btnLevel3;
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLevel1 = findViewById(R.id.btn_level1);
+        btnLevel2 = findViewById(R.id.btn_level2);
+        btnLevel3 = findViewById(R.id.btn_level3);
+
+        btnLevel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LevelsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
