cb8beafcd1f3ed88d9024cf3b835ef1de6f64a0c
Abdallah Drihem <samidrihem@gmail.com>
2019-01-31 17:31:07 +0100

    - Add LEVEL

---
+++ app/src/main/java/com/example/sami/labyrinthem2ihm/Models/Bloc.java
@@ -0,0 +1,26 @@
+package com.example.sami.labyrinthem2ihm.Models;
+
+import android.graphics.RectF;
+
+public class Bloc {
+    public enum  Type { TROU, DEPART, ARRIVEE };
+
+    private float SIZE = Bille.RAYON * 2;
+
+    private Type mType = null;
+    private RectF mRectangle = null;
+
+    public Type getType() {
+        return mType;
+    }
+
+    public RectF getRectangle() {
+        return mRectangle;
+    }
+
+    public Bloc(Type pType, int pX, int pY) {
+        this.mType = pType;
+        this.mRectangle = new RectF(pX * SIZE, pY * SIZE, (pX + 1) * SIZE, (pY + 1) * SIZE);
+    }
+}
+
